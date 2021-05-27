e65d7f6930fa7556fcf2983d4f180655f81b8483
Patrick Dorion <patrick.dorion@kellogg.nothwestern.edu>
2021-05-26 20:49:36 -0500

    added firebase auth

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
-      signInSuccessUrl: 'index.html'
+      signInSuccessUrl: 'addOrCheckCloset.html'
     }
 
     // Starts FirebaseUI Auth
