b0b3ddc6097179e6f13aa6afccf50f61ead3c604
Tony Litterio <tony@litterio.com>
2021-05-28 12:10:16 -0500

    wired up signout functionality

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
+    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+    <button class="text-pink-500 underline sign-out">Sign Out</button>
+    `
+    //refer to signout button
+    let signOutButton = document.querySelector(`.sign-out`)
+    //handle click event to sign out
+    signOutButton.addEventListener(`click`, function(event){
+      //sign out user
+      firebase.auth().signOut()
+      //redirect use to home page
+      document.location.href = `index.html`
+    })
+
   } else {
     // Signed out
     console.log('signed out')
